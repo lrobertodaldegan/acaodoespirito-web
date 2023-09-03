14c7725215e860f34c9d9b48e62eb023aa30a231
lroberto <lroberto@tribanco.com.br>
2023-09-03 12:39:45 -0300

    Atualização nas playlists e videos

--- front/acaodoespirito-web-front/src/pages/Videos/VideosPage.js
+++ front/acaodoespirito-web-front/src/pages/Videos/VideosPage.js
@@ -3,6 +3,19 @@ import './VideosPage.css'
 const VideosPage = () => {
     return (
         <div className='videos-content'>
+
+            <div className='section row'>
+                <div className='col playlist'>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/HuzARjfMKwk?si=gga00lWSpxshtmOj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+            </div>
+
+            <div className='section row'>
+                <div className='col playlist'>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=NJJLzbBCzFCEL_G2&amp;list=PL1mvKPFt_iEGMYgBnVkXqdxUvMe-t4M4k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+            </div>
+
             <div className='section row'>
                 <div className='col playlist'>
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL1mvKPFt_iEHPzYEpOZdi7JOjNR-1Ljem" title="YT Músicas" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
