2202e37972f5a4de8cca064b273ac6c15e097c97
lroberto <lroberto@tribanco.com.br>
2024-03-06 16:46:28 -0300

    Adicionando frames do Google Calendar

--- front/acaodoespirito-web-front/src/pages/Escola/EscolaPage.js
+++ front/acaodoespirito-web-front/src/pages/Escola/EscolaPage.js
@@ -18,6 +18,8 @@ const EscolaPage = () => {
                 </div>
 
                 <iframe src={pdfSobre} width='100%' height='500px' type="application/pdf"/>
+
+                <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FSao_Paulo&bgcolor=%23ffffff&showTitle=0&src=YWNhb2RvZXNwaXJpdG9pYWVAZ21haWwuY29t&src=Y2xhc3Nyb29tMTEwNzAzMzA0MDUwMTcwMzI3ODgxQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=cHQtYnIuYnJhemlsaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%233F51B5&color=%230B8043" width="800" height="600"/>
             </div>
         </div>
     );
--- front/acaodoespirito-web-front/src/pages/Home/Sections/Third/ThirdHPSection.js
+++ front/acaodoespirito-web-front/src/pages/Home/Sections/Third/ThirdHPSection.js
@@ -11,9 +11,10 @@ const ThirdHPSection = () => {
                 PRÓXIMOS EVENTOS
             </div>
 
-            <EventList />
+            {/* <EventList /> */}
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FSao_Paulo&bgcolor=%23ffffff&showTitle=0&src=YWNhb2RvZXNwaXJpdG9pYWVAZ21haWwuY29t&src=Y2xhc3Nyb29tMTEwNzAzMzA0MDUwMTcwMzI3ODgxQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=cHQtYnIuYnJhemlsaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%233F51B5&color=%230B8043" width="800" height="600"/>
 
-            <legend>Hey! Quer sabe mais? Entre em contato ou se inscreva na página de eventos!</legend>
+            <legend>Hey! Quer sabe mais? Entre em contato!</legend>
         </div>
     );
 }
